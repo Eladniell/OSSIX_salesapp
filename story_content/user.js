6b8366293a8b6df8836df8d6fbf4dcadc85cbc61
Elad Daniel <103339350+Eladniell@users.noreply.github.com>
2022-06-20 14:16:12 +0300

    V4
    V4

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,40 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6Va3fggLtaM":
+        Script1();
+        break;
+      case "6MitoXKSpwy":
+        Script2();
+        break;
+      case "5xy9sZve09i":
+        Script3();
+        break;
+      case "6UlWsl5Jeaw":
+        Script4();
+        break;
   }
+}
+
+function Script1()
+{
+  ga('send','event','videos','play','[GLYMATRIX_Video]');
+}
+
+function Script2()
+{
+  ga('send','event','videos','100% video played','[GLYMATRIX_Video]');
+}
+
+function Script3()
+{
+  var player=GetPlayer();
+var pagetitle=player.GetVar("PageTitle");
+ga('send','screenview',{screenName:pagetitle});
+}
+
+function Script4()
+{
+  ga('send','event','Button','click', OSSIX Bone');'
 }
 
