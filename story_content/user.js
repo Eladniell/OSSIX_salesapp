5f0c2477c47827a4ee87133becc9884a304ca4dd
Elad Daniel <103339350+Eladniell@users.noreply.github.com>
2022-06-22 16:31:53 +0300

    GA1

--- story_content/user.js
+++ story_content/user.js
@@ -2,40 +2,48 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5YtntSlupIX":
+      case "6J8B2gnlW1J":
         Script1();
         break;
-      case "6KtPeBd2s4J":
+      case "5rFCCX6ObvH":
         Script2();
         break;
-      case "6RiHg7Jyzm2":
+      case "6AlMp5sCsd1":
         Script3();
         break;
-      case "5WVbbe0LxSS":
+      case "6idEIeTw6LF":
         Script4();
+        break;
+      case "5hBLkOIbysk":
+        Script5();
         break;
   }
 }
 
 function Script1()
 {
-  ga('send','event','videos','play','[GLYMATRIX_Video]');
+  var player=GetPlayer();
+var pagetitle=player.GetVar("PageTitle");
+ga('send', 'screenview', {screenName: 'PageTitle'});
 }
 
 function Script2()
 {
-  ga('send','event','videos','100% video played','[GLYMATRIX_Video]');
+  ga('send', 'event', 'Button', 'click', 'OSSIX Bone');
 }
 
 function Script3()
 {
-  var player=GetPlayer();
-var pagetitle=player.GetVar("PageTitle");
-ga('send','screenview',{screenName:pagetitle});
+  ga('send', 'event', 'Button', 'click', 'OSSIX Volumax');
 }
 
 function Script4()
 {
-  ga('send','event','Button','click', OSSIX Bone');'
+  ga('send', 'event', 'Button', 'click', 'OSSIX Plus');
+}
+
+function Script5()
+{
+  ga('send', 'event', 'Button', 'click', 'GLYMATRIX');
 }
 
