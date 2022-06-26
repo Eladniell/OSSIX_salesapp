271975e676e4485d6b3014eade8d5b07d0f7dbb3
Elad Daniel <103339350+Eladniell@users.noreply.github.com>
2022-06-26 11:13:02 +0300

    260622_V2

--- story_content/user.js
+++ story_content/user.js
@@ -2,20 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6BXbHfkn8no":
+      case "6TN3b7c9xF1":
         Script1();
         break;
-      case "6rIVXAMSi4N":
+      case "6o7Owunnt4a":
         Script2();
         break;
-      case "6LQiR8Mgl7W":
+      case "6dtxTL8gaTe":
         Script3();
         break;
-      case "6bGSd8M9uTt":
+      case "6WcE9j0sqth":
         Script4();
         break;
-      case "5pWIOO86rYo":
+      case "6PT0MB9Afzg":
         Script5();
+        break;
+      case "6Tb6WH62XTC":
+        Script6();
+        break;
+      case "6SRGok1SHT8":
+        Script7();
         break;
   }
 }
@@ -23,27 +29,43 @@ function ExecuteScript(strId)
 function Script1()
 {
   var player=GetPlayer();
-var pagetitle=player.GetVar("PageTitle");
-gtag('event', 'page_view', {page_title: 'PageTitle'});
-gtag('config', 'page_view', {page_title: 'PageTitle'});
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
 }
 
 function Script2()
 {
-  gtag('send', 'event', 'Button', 'click', 'OSSIX Bone');
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'screen_view', {'screen_name': 'STLpagetitle'});
 }
 
 function Script3()
 {
-  gtag('send', 'event', 'Button', 'click', 'OSSIX Volumax');
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: '<STLpagetitle>'});
 }
 
 function Script4()
 {
-  ga('config', 'event', 'Button', 'click', 'OSSIX Plus');
+  gtag('event', 'click', {
+  'event_category' : 'userclicks',
+  'event_label' : 'OSSIX Bone'
+});
 }
 
 function Script5()
+{
+  gtag('send', 'event', 'Button', 'click', 'OSSIX Volumax');
+}
+
+function Script6()
+{
+  ga('config', 'event', 'Button', 'click', 'OSSIX Plus');
+}
+
+function Script7()
 {
   ga('send', 'event', 'Button', 'click', 'GLYMATRIX');
 }
